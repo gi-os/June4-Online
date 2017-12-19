50ca8caca7c39fa4637a054974ae153a57be7319
Giovanni Lupo <gio@gi-os.com>
2017-12-18 20:02:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,48 +1,140 @@
+//Developed by Gi-OS
+
+
 var Discordie = require("discordie");
 var Events = Discordie.Events;
-var client = new Discordie();
+var client = new Discordie({
+  autoReconnect: true,
+});
 
 //version
-var version = "v1.4.25";
+var version = "v1.6.1.3"; //version of june
+var host = "Gi-OS Servers"
+//Other important things
+var test = "FALSE"; //Is this bot avaliable for public use?
+var number = randomIntFromInterval(1,100);// do not edit please
+var token = "Mjg2Mjg4MjE0OTI5NTcxODQw.C9bF8A.OiAO_N1Gm8KIBrOQtszwa9jBZfE"//Put your token here
 //users name
-var owner = "Giovanni Lupo";
-var ownerwake = "gio"
-var ownernick = "Gio"
-var ownerusername = "@Gi-OS"
+var owner = "Giovanni Lupo"; //Your full name
+var ownerwake = "gio" //Your Nick Name Lowercase
+var ownernick = "Gio" //Your Nick Name Uppercase
+var ownerusername = "@Gi-OS" //Your User Name
+var ownerid = "279447694240382977" //Your ID
 //company
-var co = "Gi-OS"
+var co = "Gi-OS" //your company name!
 //Bot's Name
-var wake = "june";
-var nick = "June";
-var name = "June4";
+var wake = "june";//lowercase, wake word ("june" boot)
+var nick = "June"; //Nick name (Hi! I'm "June"!)
+var name = "June4";//Full name ("June4" Now powering on...)
 //other vars are somewhere else in the code (sorry! it only works there!)
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+
 client.connect({
-	token: "Mjg2Mjg4MjE0OTI5NTcxODQw.C5eiPg.-Q2fmuIV_34LLCt87KeMFI8uraQ"
+	token: token
+
 });
 
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
-	console.log(client.User.username + "4 is now awake!");
-	//game running
-	client.User.setGame("Ask me for Help");
+
+	//terminal text
+	setTimeout(function(){
+	console.log("     ██ ██    ██ ███    ██ ███████ ██   ██ "),
+	console.log("     ██ ██    ██ ████   ██ ██      ██   ██ "),
+	console.log("     ██ ██    ██ ██ ██  ██ █████   ███████ "),
+	console.log("██   ██ ██    ██ ██  ██ ██ ██           ██ "),
+	console.log(" █████   ██████  ██   ████ ███████      ██ ");
+	console.log("Booting, Please wait...");
+	setTimeout(function(){
+	console.log("════════════");
+  console.log(client.User.username + " is now awake");
+	console.log("════════════");
+	setTimeout(function(){
+	console.log("Info:");
+	setTimeout(function(){
+	console.log(version+" is currently running");
+	setTimeout(function(){
+	console.log("My name is "+name);
+	setTimeout(function(){
+	console.log("My owner is "+owner);
+	setTimeout(function(){
+	console.log("Built by Gi-OS inc.");
+	setTimeout(function(){
+	console.log("All systems nominal");
+	setTimeout(function(){
+	console.log("════════════");
+	setTimeout(function(){
+	console.log("Version is allowed for commercial use: "+test);
+	setTimeout(function(){
+	console.log("Identifier Number: "+number);
+	setTimeout(function(){
+	console.log("════════════");
+}, 100);
+}, 100);
+}, 100);
+}, 100);
+}, 100);
+}, 100);
+}, 100);
+}, 100);
+}, 100);
+}, 1000);
+}, 4000);
+}, 00);
+client.Users.get(ownerid).openDM().then(function(dm) {
+	dm.sendMessage("", false, {
+		color: 0x30bdff,
+		author: {
+
+			name: ""
+		},
+
+		fields: [{
+			name: "Good morning "+ownernick+", "+name+" is now online!",
+			value: "-Game Name set. \n-Functions ready. \n-Conection to "+owner+" found.\n-Personality Installed \n-"+"Identifier number: "+number +"\n-"+ version
+		}],
+		footer: {
+			text: " - Developed by "+co+" inc."
+		}
+
+	});
+});
+	//current game running
+	client.User.setGame("Ask me for Help!");
 });
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 
+
+//CONVERSATION
 	//Hello
 	if (e.message.content.toLowerCase() == wake + " hello") {
 		e.message.channel.sendMessage("Hi, its good to see you!");
 
+		//Wake
+	} else if (e.message.content.toLowerCase() == wake + " wake") {
+		e.message.channel.sendMessage("I'm awake!");
+
+		//thanks
+	} else if (e.message.content.toLowerCase().includes("thank")) {
+		if (e.message.content.toLowerCase().includes(wake + "")) {
+			e.message.channel.sendMessage("Anytime!");
+		} else {}
+
+//FANCY COMMANDS
 		//Status
 	} else if (e.message.content.toLowerCase().startsWith(wake + " stat")) {
 		e.message.channel.sendMessage("", false, {
 			color: 0xf441c4,
 			author: {
 
-				name: wake + "4 Status Menu"
+				name: name + " Status Menu"
 			},
 
 			fields: [{
-				name: wake + "4" + version,
-				value: "Database: Connected \nServers: Running\nPersonality: Online\n"+ name +": Ready!"
+				name: name + " " + version,
+				value: "Database: Connected \nServers: Running\nPersonality: Online\n"+ name +": Ready!"+"\nIdentifier number: "+ number +"\n-Hosted at "+ host
 
 			}],
 			footer: {
@@ -61,7 +153,7 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 
 			fields: [{
 				name: "I'm "+ name +" " + version + ",",
-				value: "Hi i'm "+ nick +", " + owner + "'s personal assistant, but I can still help you out!  Do you need any help today?'"
+				value: "Hi i'm "+ nick +", " + owner + "'s personal assistant, but I can still serve you!  Do you need any help today?"
 
 			}],
 			footer: {
@@ -69,9 +161,6 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 			}
 		});
 
-		//Wake
-	} else if (e.message.content.toLowerCase() == wake + " wake") {
-		e.message.channel.sendMessage("I'm awake!");
 
 		//Boot
 	} else if (e.message.content.toLowerCase() == wake + " boot") {
@@ -79,12 +168,12 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 			color: 0x8ff442,
 			author: {
 
-				name: nick + "4 Boot Menu"
+				name: name +  " Boot Menu"
 			},
 
 			fields: [{
 				name: "Good Morning!",
-				value: "-Game Name set. \n-Functions ready. \n-Conection to "+owner+" found.\n-Personality Installed \n-" + version
+				value: "-Game Name set. \n-Functions ready. \n-Conection to "+owner+" found.\n-Personality Installed \n-"+"Identifier number: "+number +"\n-"+ version +"\n-Hosted at "+ host
 
 			}],
 			footer: {
@@ -92,19 +181,11 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 			}
 		});
 
-		//VARS---------------------------------------------------------------
-
-		function allvar() {
-			var note = e.message.content;
-			var ownerid = client.Users.get("279447694240382977");
-			var iambluid = client.Users.get("132960971231723520");
-		}
-		//VARS END(idk why it has to be here)-------------------------------------
 
 		//tell owner
-		allvar();
+
 	} else if (e.message.content.toLowerCase().startsWith(wake + " tell "+ownerwake)) {
-		authorID = e.message.member;
+
 
 		e.message.channel.sendMessage("OK, I have notified "+ownernick, false, {
 			color: 0x30bdff,
@@ -120,19 +201,19 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 			footer: {
 				text: " - It seemed important, "+ nick
 			}
-		});
+		}),
 
-		client.Users.get("279447694240382977").openDM().then(function(dm) {
+		client.Users.get(ownerid).openDM().then(function(dm) {
 			dm.sendMessage("<@" + e.message.member + ">" + " has sent you a message.", false, {
 				color: 0x30bdff,
 				author: {
 
-					name: ""
+					name: "---"
 				},
 
 				fields: [{
-					name: e.message.content,
-					value: "⠀"
+					name: "Message Attached Below",
+					value: e.message.content
 				}],
 				footer: {
 					text: " - It seemed important, "+ nick
@@ -157,35 +238,28 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 			footer: {
 				text: " - I sent it to you, "+ nick
 			}
-		});
+})
 
-		//open notes
-	} else if (e.message.content.toLowerCase().startsWith(wake + " check note")) {
+e.message.author.openDM().then(function(dm) {
+                dm.sendMessage("", false, {
+                  color: 0xf4aa42,
+                  author: {
 
-		e.message.channel.sendMessage("", false, {
-			color: 0x30bdff,
-			author: {
+                                  name: "You requested that a note was made,"
+                              },
 
-				name: "Looking Up notes..."
-			},
-
-			fields: [{
-				name: "Please Wait",
-				value: "It will take a few moments."
-			}],
-			footer: {
-				text: " - I'm looking it up for you, "+ nick
-			}
-		});
-
-		//thanks
-	} else if (e.message.content.toLowerCase().includes("thank")) {
-		if (e.message.content.toLowerCase().includes(wake + "")) {
-			e.message.channel.sendMessage("Anytime!");
-		} else {}
+                              fields: [{
+                              name: "Your note said",
+                                  value:e.message.content
+                              }],
+                              footer: {
+                                  text: " - I saved it for you, June"
+                              }
+                          })
+});
 
 		//help menu
-	} else if (e.message.content.includes(wake + " help")) {
+	} else if (e.message.content.toLowerCase().includes(wake + " help")) {
 		e.message.channel.sendMessage("", false, {
 			color: 0x30bdff,
 			author: {
@@ -195,8 +269,9 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 
 			fields: [{
 				name: "Commands",
-				value: "-June Help\t\"Opens Help Menu\" \n-"+ nick+" Status\t\""+nick+" gives status on systems\" \n-"+nick+" Boot\t\"Opens Boot Menu\"\n-"+nick+" Tell "+ownernick+" (XXX)\t\""+nick+" sends message to "+ownerusername+"\"\n-"+nick+" note (XXX)\t\""+nick+" sends you a note.\"\n-"+nick+" Terminate\t\"Force shut down of "+nick+" (only owner can use this)\"\n-"+ nick
-				+ " Reboot\t\"reboots "+ nick+ " incase of errors (only owner can use this)\"\n-June Disconnect\t\"Disconnects "+nick+" from database (only owner can use this)\""
+				value: "-"+nick+" Help\t\"Opens Help Menu\" \n-"+ nick+" Status\t\""+nick+" gives status on systems\" \n-"+nick+" Boot\t\"Opens Boot Menu\"\n-"+nick+" About\t\"Short bio of "+nick
+				+"\"\n-"+nick+" Tell "+ownernick+" (XXX)\t\""+nick+" sends message to "+ownerusername+"\"\n-"+nick+" note (XXX)\t\""+nick+" sends you a note.\"\n-"+nick+" Alarm @XXX\t\"Notifies writer when user goes online\"\n-"
+				+nick+" Terminate\t\"Force shut down of "+nick+" (only owner can use this)\"\n-"+ nick + " Reboot\t\"reboots "+ nick+ " incase of errors (only owner can use this)\"\n-"+nick+" Disconnect\t\"Disconnects "+nick+" from database (only owner can use this)\""
 
 			}],
 			footer: {
@@ -204,18 +279,41 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 			}
 		});
 
-		//test
-	} else if (e.message.content.toLowerCase().includes(wake + " test")) {
-		client.Dispatcher.on("PRESENCE_UPDATE", e => {
-		    console.log("Something!");
-		});
+//Info
+} else if (e.message.content.toLowerCase().includes(wake + " info")) {
 
-		//alarm users
-	} else if (e.message.content.toLowerCase().includes(wake + " alarm")) {
+e.message.channel.sendMessage("", false, {
+	color: 0x30bdff,
+	author: {
 
-		//TERMINATE COMMANDS
+		name: ""
+	},
+
+	fields: [{
+		name: "OK, lets see...",
+		value: "My Identification Number is "+number+"\nMy Version is "+ version+"\nAnd I am hosted at "+ host
+	}],
+	footer: {
+		text: "There you Go!"
+	}
+});
+} else if (e.message.content.toLowerCase().includes(wake + " server")) {
+var list = "**Server List:** \r\n";
+var numb = 1;
+console.log("Recieved Servers command \r\n");
+client.Guilds.map((self) => {
+	list += numb + ") " + self.name + "\r\n";
+	numb = numb+1;
+
+});
+	e.message.channel.sendMessage(list);
+
+
+
+		//ADMIN COMMANDS
+		//disconnect
 	} else if (e.message.content.toLowerCase().startsWith(wake + " disconnect")) {
-		if (e.message.author == client.Users.get("279447694240382977")) {
+		if (e.message.author == client.Users.get(ownerid)) {
 			e.message.channel.sendMessage("", false, {
 				color: 0xf44242,
 				author: {
@@ -232,84 +330,19 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 					text: " - "+ name +" built by "+co+" ©2017 "
 				}
 			});
-			console.log(client.User.username + "4 is now being disconnected.");
+			console.log(client.User.username + " is now being disconnected.");
 			setTimeout(function() {
 				client.disconnect({
-					token: "Mjg2Mjg4MjE0OTI5NTcxODQw.C5eiPg.-Q2fmuIV_34LLCt87KeMFI8uraQ"
+					token: token
 				});
 
 			}, 500);
 			setTimeout(function() {
-				console.log(client.User.username + "4 is now disconnected.");
+				console.log(client.User.username + " is now disconnected.");
 			}, 1000);
 
 		} else {
-			e.message.channel.sendMessage("", false, {
-				color: 0xf44242,
-				author: {
-
-					name: "You do not have permission to run this command"
-				},
-
-				fields: [{
-
-					value: "The process will not continue",
-					name: "I\'Only "+ownerusername+" can use this command."
-				}],
-				footer: {
-					text: " - "+ name +" built by "+co+" ©2017 "
-				}
-			});
-		}
-	} else if (e.message.content.toLowerCase().startsWith(wake + " reboot")) {
-		if (e.message.author == client.Users.get("279447694240382977")) {
-			e.message.channel.sendMessage("", false, {
-				color: 0xf44242,
-				author: {
-
-					name: name +" is now rebooting."
-				},
-
-				fields: [{
-
-					value: "The reboot process will take a few seconds.",
-					name: "I\'ll see you soon!"
-				}],
-				footer: {
-					text: " - "+ name +" built by "+co+" ©2017 "
-				}
-			});
-			console.log(client.User.username + "4 is now rebooting.");
-			setTimeout(function() {
-
-				client.disconnect({
-					token: "Mjg2Mjg4MjE0OTI5NTcxODQw.C5eiPg.-Q2fmuIV_34LLCt87KeMFI8uraQ"
-				});
-
-			}, 500);
-			setTimeout(function() {
-				client.connect({
-					token: "Mjg2Mjg4MjE0OTI5NTcxODQw.C5eiPg.-Q2fmuIV_34LLCt87KeMFI8uraQ"
-				});
-				e.message.channel.sendMessage("", false, {
-					color: 0x8ff442,
-					author: {
-
-						name: name +" Boot Menu"
-					},
-
-					fields: [{
-						name: "Good Morning!",
-						value: "-Game Name set. \n-Functions ready. \n-Conection to "+owner+" found.\n-Personality Installed\n-" + version
-
-					}],
-					footer: {
-						text: " - "+ name +" built by "+co+" ©2017 "
-					}
-				});
-			}, 10000);
-
-		} else {
+					console.log("someone tried to disconnect me!");
 			e.message.channel.sendMessage("", false, {
 				color: 0xf44242,
 				author: {
@@ -327,8 +360,80 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 				}
 			});
 		}
+		//reboot
+	} else if (e.message.content.toLowerCase().startsWith(wake + " reboot")) {
+		if (e.message.author == client.Users.get(ownerid)) {
+			e.message.channel.sendMessage("", false, {
+				color: 0xf44242,
+				author: {
+
+					name: name +" is now rebooting."
+				},
+
+				fields: [{
+
+					value: "The reboot process will take a few seconds.",
+					name: "I\'ll see you soon!"
+				}],
+				footer: {
+					text: " - "+ name +" built by "+co+" ©2017 "
+				}
+			});
+			console.log(client.User.username + " is now rebooting.");
+			setTimeout(function() {
+
+				client.disconnect({
+					token: token
+				});
+
+			}, 500);
+			setTimeout(function() {
+				client.connect({
+					token: token
+				});
+				e.message.channel.sendMessage("", false, {
+					color: 0x8ff442,
+					author: {
+
+						name: name +" Reboot Menu"
+					},
+
+					fields: [{
+						name: "Good Morning!",
+						value: "-Game Name set. \n-Functions ready. \n-Conection to "+owner+" found.\n-Personality Installed\n-" + version
+
+					}],
+					footer: {
+						text: " - "+ name +" built by "+co+" ©2017 "
+					}
+				});
+			}, 10000);
+
+		} else {
+					console.log("someone tried to reboot me!");
+			e.message.channel.sendMessage("", false, {
+				color: 0xf44242,
+				author: {
+
+					name: "You do not have permission to run this command"
+				},
+
+				fields: [{
+
+					value: "The process will not continue",
+					name: "Only "+ownerusername+" can use this command."
+				}],
+				footer: {
+					text: " - "+ name +" built by "+co+" ©2017 "
+				}
+			});
+		}
+		//terminate (important:very dangerous command, hard crashes the system.)
 	} else if (e.message.content.toLowerCase().startsWith(wake + " terminate")) {
-		if (e.message.author == client.Users.get("279447694240382977")) {
+
+if (e.message.author == client.Users.get(ownerid)) {
+		if (e.message.content.toLowerCase().includes(number)) {
+			if (e.message.author == client.Users.get(ownerid)) {
 			e.message.channel.sendMessage("", false, {
 				color: 0xf44242,
 				author: {
@@ -345,12 +450,12 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 					text: " - "+ name +" built by "+co+" ©2017 "
 				}
 			});
-			console.log(client.User.username + "4 is now closing.");
+			console.log(client.User.username + " is now closing.");
 			setTimeout(function() {
 				killswitch();
 			}, 500);
-
 		} else {
+					console.log("someone tried to terminate me!");
 			e.message.channel.sendMessage("", false, {
 				color: 0xf44242,
 				author: {
@@ -369,7 +474,48 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 			});
 		}
 
-	} else {
+} else {
+	e.message.channel.sendMessage("", false, {
+		color: 0xf44242,
+		author: {
 
-	}
+			name: ""+ name +" Identifier Number not given"
+		},
+
+		fields: [{
+
+			value: "My Identifier Number: "+number,
+			name: "⠀"
+		}],
+		footer: {
+			text: " - "+ name +" built by "+co+" ©2017 "
+		}
+	});
+}
+
+} else {
+  console.log("someone tried to terminate me!");
+e.message.channel.sendMessage("", false, {
+color: 0xf44242,
+author: {
+
+  name: "You do not have permission to run this command"
+},
+
+fields: [{
+
+  value: "The process will not continue",
+  name: "Only "+ownerusername+" can use this command."
+}],
+footer: {
+  text: " - "+name+" built by "+co+" ©2017 "
+}
 });
+}
+
+
+
+
+}else{}
+
+})
